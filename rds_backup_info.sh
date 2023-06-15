4faa3105f8f7b59bedca451661b14fd38aa18765
push1252 <114217972+push1252@users.noreply.github.com>
2023-06-15 15:54:21 +0530

    Update rds_backup_info.sh

--- rds_backup_info.sh
+++ rds_backup_info.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
-rds_instances=("gromo-staging-rds" "gromo-production-rds" "gromo-production-replica-rds")
+rds_instances=("staging-rds" "production-rds")
 
 for rds_instance in "${rds_instances[@]}"; do
-    backup=$(aws rds describe-db-instances --db-instance-identifier "$rds_instance" --query 'DBInstances[0].BackupRetentionPeriod' --output text --profile gromoappbackend)
+    backup=$(aws rds describe-db-instances --db-instance-identifier "$rds_instance" --query 'DBInstances[0].BackupRetentionPeriod' --output text --profile pushpit)
 
     if [ "$backup" -gt 0 ]; then
         echo "Backup is enabled for $rds_instance"
