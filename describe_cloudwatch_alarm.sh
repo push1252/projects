60cf29e55f605e8ceb68f0481e8abdeb8ec1ceb1
push1252 <114217972+push1252@users.noreply.github.com>
2023-06-15 15:52:03 +0530

    Update describe_cloudwatch_alarm.sh

--- describe_cloudwatch_alarm.sh
+++ describe_cloudwatch_alarm.sh
@@ -4,7 +4,7 @@
 AWS_REGION="your-aws-region"
 
 # Get a list of all EC2 instances in the region
-EC2_INSTANCE_IDS=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --output text --profile gromoinsure)
+EC2_INSTANCE_IDS=$(aws ec2 describe-instances --query 'Reservations[*].Instances[*].InstanceId' --output text --profile pushpit
 
 # Loop through each EC2 instance
 for INSTANCE_ID in $EC2_INSTANCE_IDS; do
