832780447b8a918058a71cf5784963bc8855249a
push1252 <114217972+push1252@users.noreply.github.com>
2023-06-15 15:53:33 +0530

    Update myip-update.sh

--- myip-update.sh
+++ myip-update.sh
@@ -7,7 +7,7 @@ region="ap-south-1"
 security_group_name="jenkins(security_group)"
 #load_balancer_name="albpushpitjenkins"
 
-security_group_id=$(aws ec2 describe-security-groups --filters Name=group-name,Values="jenkins(security_group)" --query "SecurityGroups[0].GroupId" --output text --profile gromo-app-back)
+security_group_id=$(aws ec2 describe-security-groups --filters Name=group-name,Values="jenkins(security_group)" --query "SecurityGroups[0].GroupId" --output text --profile pushpit)
 
 
-aws ec2 authorize-security-group-ingress --group-id sg-068a5fad8603dd055 --protocol all --port 443 --cidr $myip/32 --profile gromo-app-back
+aws ec2 authorize-security-group-ingress --group-id sg-068a5fad8603dd055 --protocol all --port 443 --cidr $myip/32 --profile pushpit
